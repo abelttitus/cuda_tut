173d6b209613314d4f0f562ebbca3e53aa209eb0
abelttitus <abelttitus1998@gmail.com>
2020-05-12 15:04:38 +0530

    second

--- vector_add.cu
+++ vector_add.cu
@@ -8,13 +8,13 @@
 #define N 10000000
 #define MAX_ERR 1e-6
 
-__global__ vector_add(float* out,float* a,float* b,int n){
+__global__ void vector_add(float* out,float* a,float* b,int n){
 	for(int i=0;i<n;i++){
 		out[i]=a[i]+b[i];
 	}
 	}
 
-void main(){
+int main(){
 	float *a,*b,*out;
 	float *d_a,*d_b,*d_out;
 
@@ -22,7 +22,7 @@ void main(){
 	b=(float*)malloc(sizeof(float)*N);
 	out=(float*)malloc(sizeof(float)*N);
 
-	for(int i=0;i<N;i++{
+	for(int i=0;i<N;i++){
 		a[i]=1.0f;
 		b[i]=2.0f;
 	}
